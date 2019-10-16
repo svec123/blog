9c017a7e113aa43f18aca4b53fd58a5287df2367
Vitaly V <svec1976@mail.ru>
2019-10-16 19:48:58 +0500

    Пагинация и динамические статьи из базы

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace app\controllers;
-
+use app\models\Article;
+use app\models\Category;
+use app\models\CommentForm;
 use Yii;
+use yii\data\Pagination;
 use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
@@ -61,7 +63,35 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+
+        // $data = Article::getAll();
+        // $popular = Article::getPopular();
+        // $recent = Article::getRecent();
+        // $categories = Category::getAll();
+        
+        // return $this->render('index',[
+        //     'articles'=>$data['articles'],
+        //     'pagination'=>$data['pagination'],
+        //     'popular'=>$popular,
+        //     'recent'=>$recent,
+        //     'categories'=>$categories
+        // ]);
+            
+
+        $query = Article::find();
+
+        $count = $query->count();
+
+        $pagination = new Pagination(['totalCount'=>$count , 'pageSize'=>1]);
+
+        $articles = $query->offset($pagination->offset)
+        ->limit($pagination->limit)
+        ->all();
+
+        return $this->render('index',[
+            'articles'=>$articles,
+            'pagination'=>$pagination
+        ]);
     }
 
     /**
